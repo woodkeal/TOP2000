f5fefd27dc4d96d4079c07bfe9a8efb67fcea0b4
tim houtman <timhoutman1999@gmail.com>
2021-05-28 10:41:30 +0200

    Aanmaken van het project

---
+++ TOP2000/TOP2000/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TOP2000.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
